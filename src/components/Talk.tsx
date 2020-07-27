86f3abac2e016f52e0e06bae5a517a70e3ecf4f6
Aaron Powell <me@aaron-powell.com>
2020-07-27 21:46:25 +1000

    having some fun with styles

--- src/components/Talk.tsx
+++ src/components/Talk.tsx
@@ -1,13 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 
 type TalkProps = {
   title: string;
   speaker: string;
+  room: string;
 };
 
-const Talk: React.FC<TalkProps> = ({ title, speaker }) => (
-  <p>
-    {title} - {speaker}
-  </p>
+const TalkContainer = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  font-family: "Cascadia Code PL";
+  min-height: 350px;
+  position: relative;
+
+  &:hover {
+    background-color: #fff;
+    color: #282c34;
+    border-color: #282c34;
+  }
+`;
+
+const TalkTitle = styled.p`
+  font-size: 26px;
+`;
+const Speaker = styled.p`
+  font-size: 20px;
+`;
+const Room = styled.p`
+  font-size: 16px;
+  position: absolute;
+  bottom: 0;
+`;
+
+const Talk: React.FC<TalkProps> = ({ title, speaker, room }) => (
+  <TalkContainer>
+    <TalkTitle>{title}</TalkTitle>
+    <Speaker>{speaker}</Speaker>
+    <Room>{room}</Room>
+  </TalkContainer>
 );
 export default Talk;
--- src/components/Timeslot.tsx
+++ src/components/Timeslot.tsx
@@ -1,19 +1,32 @@
 import React from "react";
 import { Timeslot } from "../fetchAgenda";
 import Talk from "./Talk";
-import { SectionHeader } from "./headers"
+import { SectionHeader } from "./headers";
+import styled from "styled-components";
+
+const renderTime = ({ hour, minutes }: { hour: number; minutes: number }) =>
+  `${hour.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-content: space-evenly;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+`;
 
 const TimeslotComponent: React.FC<{ timeslot: Timeslot }> = ({ timeslot }) => {
   const sessions = timeslot.sessions;
   return (
     <li>
       <SectionHeader>
-        {timeslot.startTime.hour}:{timeslot.startTime.minutes} -{" "}
-        {timeslot.endTime.hour}:{timeslot.endTime.minutes}
+        {renderTime(timeslot.startTime)} - {renderTime(timeslot.endTime)}
       </SectionHeader>
-      {sessions.map(({ title, speaker }) => (
-        <Talk title={title} speaker={speaker} key={title} />
-      ))}
+      <Grid>
+        {sessions.map(({ title, speaker, location }) => (
+          <Talk title={title} speaker={speaker} key={title} room={location} />
+        ))}
+      </Grid>
     </li>
   );
 };
